5cc27b848741f00b2baceabdbb395048b73183f3
Makay131 <mahmutenesakay9@gmail.com>
2023-10-26 23:13:56 +0300

    swiper added for gallery section, useEffect practice

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,27 +4,57 @@ import GalleryItem from "../interface/GalleryItem";
 const dummyData = [
     {
         src: './pics/antarctica.jpg',
-        id: 1
+        id: 1,
+        slideList: [
+            './pics/dubai.jpg',
+            './pics/hawaii.jpg',
+            './pics/iceland.jpg',
+        ]
     },
     {
         src: './pics/dubai.jpg',
-        id: 2
+        id: 2,
+        slideList: [
+            './pics/california.jpg',
+            './pics/belize.webp',
+            './pics/iceland.jpg',
+        ]
     },
     {
         src: './pics/hawaii.jpg',
-        id: 3
+        id: 3,
+        slideList: [
+            './pics/belize.webp',
+            './pics/iceland.jpg',
+            './pics/antarctica.jpg',
+        ]
     },
     {
         src: './pics/iceland.jpg',
-        id: 4
+        id: 4,
+        slideList: [
+            './pics/dubai.jpg',
+            './pics/hawaii.jpg',
+            './pics/iceland.jpg',
+        ]
     },
     {
         src: './pics/california.jpg',
-        id: 5
+        id: 5,
+        slideList: [
+            './pics/california.jpg',
+            './pics/belize.webp',
+            './pics/iceland.jpg',
+        ]
     },
     {
         src: './pics/belize.webp',
-        id: 6
+        id: 6,
+        slideList: [
+            './pics/california.jpg',
+            './pics/belize.webp',
+            './pics/iceland.jpg',
+        ]
     },
 ];
 
--- src/interface/GalleryItem.jsx
+++ src/interface/GalleryItem.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react"
 import BaseButton from "./BaseButton"
+import { createPortal } from "react-dom";
+import ModalContent from "../components/ModalContent";
+import SwiperComponent from "../components/SwiperComponent";
 
 function GalleryItem({data}) {
+    const [showSlides, setShowSlides] = useState(false);
     return (
         <div className="box-gallery overflow-hidden shadow-xravel-shadow-1 border-[1rem] border-white rounded-[0.5rem] h-[25rem] relative">
             <img className="h-full w-full object-cover" src={data.src} alt="//TODO:" />
             <div className="gallery-content absolute -top-full left-0 h-full w-full flex items-center justify-center bg-xravel-color-overlay-1 p-[2rem] pt-[3.8rem] duration-300">
-                <BaseButton>See More</BaseButton>
+                <BaseButton onButtonClicked={() => setShowSlides(true)}>See More</BaseButton>
             </div>
+            {showSlides && createPortal(<ModalContent>
+                <SwiperComponent slideList={data.slideList} onShowSlides={setShowSlides} />
+            </ModalContent>, document.body)}
         </div>
     )
 }
